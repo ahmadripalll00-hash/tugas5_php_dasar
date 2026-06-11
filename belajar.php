5fa45d4b1f92b8fbb58a17433a4df94d7490763b
Rifal <ahmadripal.ll00@gmail.com>
2026-06-11 23:41:43 +0700

    membuat file php dengan sintax dasar html dan php

--- belajar.php
+++ belajar.php
@@ -1,3 +1,12 @@
-<?php
-
-echo "Helloword"
+<!DOCTYPE html>
+<html>
+<head>
+    <title><?php echo "Belajar PHP" ?></title>
+</head>
+<body>
+    <?php
+    echo "saya sedang belajar PHP<br>";
+    echo "<p>Belajar PHP hingga jadi master</p>";
+    ?>
+</body>
+</html>
